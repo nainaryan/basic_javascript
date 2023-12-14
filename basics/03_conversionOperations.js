d9ed370c460b1a7dcbe10ac2f8972612dd759b74
ARYAN NAIN <98951969+nainaryan@users.noreply.github.com>
2023-12-14 10:30:07 +0000

    operations on conversion

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33ab"
+let a = null 
+console.log(typeof score);
+
+let valueInNumber = Number(score)       // first alphabet of datatype is CAPITAL ...
+
+console.log(typeof valueInNumber);    // number
+console.log(valueInNumber);     // NaN : NOT A NUMBER ( SPECIAL TYPE )
+
+
+/*
+ "33" => 33
+ "33abc " => NaN
+ true => 1 , false => 0 
+
+ */
+
+let someNumber = 33 
+
+let valueInString = String( someNumber )
+console.log( valueInString );
+console.log( typeof (valueInString ) )
+
+
+
---
+++ basics/04_operations.js
@@ -0,0 +1,22 @@
+let value = 3  
+
+let negValue = -value
+console.log(negValue);
+console.log(2**3);  // 2**3 = > 2^3 . 
+
+let str1 = "hello"
+let str2 = " aryan"
+console.log( str1 + str2 )  // concatenating string 
+
+console.log("1"+ 2 ) ;      //12
+console.log(1 + "2" ) ;     //12
+console.log("1"+ 2 + 2) ;   //122
+console.log(1+ 2 + "2" ) ;  //32
+ 
+// agar string last mei hai toh pahle ke saare conversions ho jaayenge ...
+// agar string 1st par hai toh sabko string banaa diya jaayega ...
+
+console.log(+true);
+console.log(+"");
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2 + 2 ;
