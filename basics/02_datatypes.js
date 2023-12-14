e8162571e532652714b4e986f53064303f6b7f20
ARYAN NAIN <98951969+nainaryan@users.noreply.github.com>
2023-12-14 09:24:03 +0000

    basic datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "aryan " // string 
+let age = 18        // number
+
+let isvalid = true  // boolean 
+let state;   // undefined 
+
+/*
+    symbol => unique 
+    oblect 
+    datatype of null is object .
+*/
+
+console.log( typeof "aryan ")       // string
+console.log( typeof age );          // number
+console.log( typeof null );         // oblect 
+console.log( typeof undefined);    // undefined
+
