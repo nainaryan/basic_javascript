abde489eab5c7107cf55253ae5e1a31e37582191
ARYAN NAIN <10aryannain@gmail.com>
2023-12-16 12:49:37 +0530

    basic functons of strings done

---
+++ basics/06_strings.js
@@ -0,0 +1,41 @@
+const name = "aryan "
+const repocount = 50 
+// console.log( name + repocount + "value ");   // [ not recommended for concat. ]
+
+
+// STRING INTERPOLATION ---> backticks(` ${}` ) // modern way 
+
+console.log(`Hello my name is ${name}and my repo count is ${repocount} `);   
+
+const gameName = new String ('aryan-nain')    // first word capital 
+
+console.log(gameName[0]);       //a 
+console.log(gameName.length);       //10
+console.log(gameName.toUpperCase()); // ARYANNAIN
+console.log(gameName.charAt('2')); // y    indexOf('y') => 2
+
+//*******************************substring() **************************************************************
+const newString = gameName.substring(0,4);  // do not obey negative values 
+console.log(newString); //arya [ last wala index include nahi hua ]
+
+//*****************************slice() *********************
+const anotherstring = gameName.slice( -10 , 4 )   // you can use negative vlaues 
+console.log(anotherstring); // arya 
+
+//*******************************trim()********************************************************************
+const newString1 = "   aryan   "
+console.log(newString1);            //   aryan   .
+console.log(newString1.trim());     //aryan. // it will remove unnecesaary space 
+
+//**************** *****************replace() *************************************************************
+
+const url = "https://aryan.com/aryan%20nain"
+console.log(url.replace('%20' , '-'));    // https://aryan.com/aryan-nain
+
+console.log(url.includes('aryan'))        // true , it will return whether a string contain this word or not
+
+
+//**********************************split()*****************************************************************
+const newString2 = "aryan-nain-21-modeltown"
+const newarray = newString2.split('-')
+console.log(newarray);  //[ 'aryan', 'nain', '21', 'modeltown' ] 
