64a50fccdeb9abc687462f3eccae8f60c0b043e6
nainaryan <10aryannain@gmail.com>
2024-05-14 10:30:05 +0530

    function basics 1

--- basics_3/01_functions.js
+++ basics_3/01_functions.js
@@ -1,1 +1,41 @@
-// function basics
+// function in javascript 
+// code ko package mei band kardena
+
+
+function sayMyName()
+{
+    console.log("C");
+    console.log("A");
+    console.log("T") ;
+}
+// sayMyName //--> reference of function 
+// sayMyName()     // C           A           T
+
+function addTwoNumbers(number1 , number2 )   // -- > parameters
+{
+    console.log(number1 + number2 ); 
+}
+const result1 = addTwoNumbers(5,"6")  // 56    --> arguments
+addTwoNumbers(3,"a")  //3a 
+
+console.log( " result1 : ",result1); // result1 : undefined
+
+function add(number1 , number2)
+{
+    return number1 + number2
+    console.log("aryan");  // unreachable quote ...
+}
+const result2= add(7,8)
+console.log(" result2 : " , result2); //  result2 : 15 
+
+function loginUserMessage(username)
+{
+    return `${username} just logged in `
+}
+loginUserMessage("aryan nain "); // kuch nahi hoga idhar 
+console.log(loginUserMessage(" thala "));   //  thala  just logged in 
+
+console.log(loginUserMessage());   // undefined just logged in      
+// you can use if statement with strict equality condition within a function
+
+// "", undefined , 0 , ... --> false in js 
