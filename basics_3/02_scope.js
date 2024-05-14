4093a8d0b4941b292c2edaef4350bf550263875a
nainaryan <10aryannain@gmail.com>
2024-05-14 11:13:20 +0530

    scope levels and expressions

--- basics_3/02_scope.js
+++ basics_3/02_scope.js
@@ -15,4 +15,45 @@ console.log(d); // 100
 // console.log(b);   // ! error -- b is not defined 
 console.log(c); // bkl var --> 30 print ho jayega
 
+/***************************************************************************************** */
 
+// nested function 
+
+function one(){
+    const username = "aryan"
+
+    function two(){
+        const website = "youtube.com"
+        console.log(username);   // chota bachha badde se ice cream maang sakta hai 
+    }
+    // console.log(website);  -- > !error 
+    two() // aryan   ( two yahaa se execute hoga )
+}
+
+one()
+
+if(true)
+{
+    const username = "aryan nain"
+    if(username === "aryan nain")
+    {
+        const website = "->youtube"
+        console.log(username + website);  //aryan nain->youtube
+    }
+}
+
+/*************************** INTERSETING ********************************************* */
+
+console.log(addone(5));  // 6 koi error nahi aayega
+
+function addone(num)
+{
+    return num + 1 ;
+}
+
+
+// addTwo(5)   // !ERROR cannot access addTwo before intialisationn 
+const addTwo = function(num){ // function or expressions 
+    return num+2
+}
+// addTwo(5) // ye toh koi error nahi dega 
