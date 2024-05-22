c0b96b1d08b57c1ed4ca9fc9995b050cf158dc87
nainaryan <10aryannain@gmail.com>
2024-05-22 15:19:09 +0530

    switch case and break statement

---
+++ control flow/two.js
@@ -0,0 +1,21 @@
+//switch case statements 
+// break ; keyword importance 
+
+let month = 3
+switch (month) {
+    case 1:
+        console.log("january");        
+        break;
+    case 2:
+        console.log("february");        
+        break;
+    case 3:
+        console.log("march");        
+        break;
+    case 4:
+        console.log("april");        
+        break;
+
+    default:    console.log("humko naa pataa");
+        break;
+}
