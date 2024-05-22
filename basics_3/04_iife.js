9bdc79dc15efa3143bd02f8b829ab6b9e052318e
nainaryan <10aryannain@gmail.com>
2024-05-22 15:07:37 +0530

    iife and 2 iife in same program

--- basics_3/04_iife.js
+++ basics_3/04_iife.js
@@ -1,35 +1,10 @@
-let val1 = 10 
-let val2 = 5
-function addNum( num1 , num2 ){
-    let total = num1+num2 
-    return total 
-}
-let result1= addNum(val1,val2)
-
-let result2= addNum(10,2 )
-
-
-
-
-
 // immediately invoked functino expression ( IIFE )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
+(function chai(){
+    console.log(`DB CONNECT`);
+})();
+//use semicolon before using 2nd iife 
+//iife in arrow function 
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})('aryan nain')
