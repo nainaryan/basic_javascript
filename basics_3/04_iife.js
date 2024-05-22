332bedc9ae30e84832573524565299bfa6972002
nainaryan <10aryannain@gmail.com>
2024-05-22 13:16:34 +0530

    iife basic program

--- basics_3/04_iife.js
+++ basics_3/04_iife.js
@@ -1,1 +1,35 @@
+let val1 = 10 
+let val2 = 5
+function addNum( num1 , num2 ){
+    let total = num1+num2 
+    return total 
+}
+let result1= addNum(val1,val2)
+
+let result2= addNum(10,2 )
+
+
+
+
+
 // immediately invoked functino expression ( IIFE )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
